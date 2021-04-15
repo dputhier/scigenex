7130b3d26b54cdfba42b5402bcc53f5e438375eb
dputhier <puthier@gmail.com>
2021-04-15 17:23:13 +0200

    Changed get_data_4_DBFMCL to snake case

--- tests/testthat/test.dbfmcl.R
+++ tests/testthat/test.dbfmcl.R
@@ -1,11 +1,11 @@
-test_that("Cheking DBFMCL is providing the right number of clusters", {
+test_that("Cheking DBFMCL is providing the right number of genes", {
   set.seed(123)
   m <- matrix(rnorm(80000), nc=20)
   m[1:100,1:10] <- m[1:100,1:10] + 4
   m[101:200,11:20] <- m[101:200,11:20] + 3
   m[201:300,5:15] <- m[201:300,5:15] + -2
   res <- DBFMCL(data=m,
-                distance.method="pearson",
+                distance_method="pearson",
                 clustering=FALSE,
                 k=25)
   expect_equal(length(res@size), 1)
