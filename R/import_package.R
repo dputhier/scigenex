d2556c439c4885ac0d284a2d2433a36407b0f23e
bavais <julie.bavais@yahoo.fr>
2021-09-08 16:46:20 +0200

    Add R file with imported function from dependencies

---
+++ R/import_package.R
@@ -0,0 +1,17 @@
+# @importClassesFrom methods character list matrix vector numeric
+#' @importMethodsFrom methods show
+#' 
+#' @importFrom methods new slot slotNames setClass signature setMethod setGeneric
+#' @importFrom grDevices colorRampPalette
+#' @importFrom stats sd dist hclust heatmap median rnorm setNames
+#' @importFrom utils object.size read.table write.table read.csv
+#' @importFrom dplyr summarise group_by distinct
+#' @importFrom ggplot2 ggplot theme_bw facet_grid geom_line aes theme scale_fill_gradientn geom_tile element_text element_blank geom_vline scale_color_manual guide_legend stat_density scale_linetype_manual scale_size_manual element_line xlab ylab geom_text
+#' @importFrom reshape2 melt 
+#' @importFrom magrittr %>%
+#' @importFrom testthat expect_equal
+#' @importFrom igraph graph_from_data_frame as_adj
+#' @importFrom MCL mcl
+#' 
+#' 
+NULL
