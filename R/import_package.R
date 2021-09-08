8523c942ee9d9ccbfe01f26f27281626317ed8bd
bavais <julie.bavais@yahoo.fr>
2021-09-08 17:34:13 +0200

    Improve import_packages.R readability

--- R/import_package.R
+++ R/import_package.R
@@ -6,7 +6,9 @@
 #' @importFrom stats sd dist hclust heatmap median rnorm setNames
 #' @importFrom utils object.size read.table write.table read.csv
 #' @importFrom dplyr summarise group_by distinct
-#' @importFrom ggplot2 ggplot theme_bw facet_grid geom_line aes theme scale_fill_gradientn geom_tile element_text element_blank geom_vline scale_color_manual guide_legend stat_density scale_linetype_manual scale_size_manual element_line xlab ylab geom_text
+#' @importFrom ggplot2 ggplot geom_line aes geom_tile geom_vline geom_text
+#' @importFrom ggplot2 theme theme_bw element_text element_blank element_line facet_grid guide_legend stat_density xlab ylab
+#' @importFrom ggplot2 scale_fill_gradientn scale_color_manual scale_linetype_manual scale_size_manual 
 #' @importFrom reshape2 melt 
 #' @importFrom magrittr %>%
 #' @importFrom testthat expect_equal
