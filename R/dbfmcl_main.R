b8d316a8871fe04b960441a1a29c76c6be3e249e
bavais <julie.bavais@yahoo.fr>
2021-09-10 14:55:47 +0200

    Add cluster parameter in top_genes function + Compute distance based on the distance used by DBFMCL

--- R/dbfmcl_main.R
+++ R/dbfmcl_main.R
@@ -1475,6 +1475,8 @@ get_data_4_DBFMCL <- function(data = NULL, filename = NULL, path = ".") {
 #' @description
 #' Extract the highly co-expressed genes of each gene cluster.
 #' @param object A \code{ClusterSet} object.
+#' @param top A value
+#' @param cluster A vector
 #'
 #' @return 
 #' @export top_genes
@@ -1490,45 +1492,77 @@ get_data_4_DBFMCL <- function(data = NULL, filename = NULL, path = ".") {
 #' 
 
 top_genes <- function(object,
-                      top = 10) {
+                      top = 20,
+                      cluster = "all") {
+  
+  loop <- 0
+  for (size in object@size){
+    loop <- 1 + loop
+    if(top > size) {
+      warning(paste0("Number of top genes is greater than the number of genes in cluster ", loop, ". All genes will be use and order by similarity rank."))
+    }
+  }
   
   # Initialization for the for loop
-  cluster <- object@cluster
+  clusters <- object@cluster
   l_cor_means <- list()
   genes_top <- matrix(ncol = top)
   
+  if(unique(cluster == "all")) {
+    cluster <- c(1:length(object@size))
+  }
+  
   # Extract top co-expressed genes for each gene cluster
-  for (i in 1: length(object@size)) {
+  for (i in cluster) {
     #Extract gene names in cluster i
-    genes <- names(cluster[which(cluster == i)])
+    genes <- names(clusters[which(clusters == i)])
     
-    #Compute correlations between genes in cluster i
-    cor_genes <- cor(t(object@data[genes,]), method = "pearson") # /!\ UTILISER LA DISTANCE CONTENU DANS LE ClusterSet OBJECT /!\ res_gene@parameters$distance_method
+    #Compute distances between genes in cluster i
+    #Use the same distance used by DBFMCL
+    dist_method <- object@parameters$distance_method
+    
+    if (dist_method == "pearson") {
+      cor_genes <- cor(t(object@data[genes,]), method = "pearson")
+      dist <- 1 - cor_genes
+    }
+    
+    if(dist_method == "spearman") {
+      cor_genes <- cor(t(object@data[genes,]), method = "spearman")
+      dist <- 1 - cor_genes
+    }
+    
+    if(dist_method == "euclidean") {
+      dist <- as.matrix(dist(object@data[genes,], method = "euclidean", upper = TRUE, diag = TRUE))
+      diag(dist) <- NA
+    }
+    
     
     #Compute mean correlation for each gene in cluster i
-    cor_means <- colMeans(cor_genes)
-    cor_means <- cor_means[order(cor_means, decreasing = TRUE)]
+    dist_means <- colMeans(dist, na.rm = TRUE)
+    dist_means <- dist_means[order(dist_means, decreasing = FALSE)]
     
     #Extract top genes with the highest correlation mean
-    genes_top <- rbind(genes_top, names(cor_means[1:top]))
+    genes_top <- rbind(genes_top, names(dist_means[1:top]))
   }
   
   # Prepare genes_top matrix
-  genes_top <- genes_top[2:(length(object@size)+1),]
+  if (length(cluster) > 1) {
+    genes_top <- genes_top[2:(length(cluster)+1),]
+  } else {
+    genes_top <- as.matrix(t(genes_top[2:(length(cluster)+1),]))
+  }
   colnames(genes_top) <- make.names(rep("gene.top", top+1), unique=TRUE)[2:(top+1)]
-  rownames(genes_top) <- make.names(rep("cluster", length(object@size)+1), unique=TRUE)[2:(length(object@size)+1)]
+  rownames(genes_top) <- make.names(rep("cluster", length(cluster)+1), unique=TRUE)[2:(length(cluster)+1)]
   
   # Put genes_top matrix in object@top_genes
   object@top_genes <- genes_top
   
+  # Print
+  print_msg(msg = "Results are stored in top_genes slot of ClusterSet object.",
+            msg_type = "INFO")
+  
   return(object)
 }
-
-
-
-
-
-
 
 
 #########################################################
