ae8ce3eae5d93ac0fbe0897844a74c52dd2fe801
Julie BAVAIS <julie.bavais@yahoo.fr>
2023-02-21 10:41:09 +0100

    Reformat code of tests for print_msg and print_stat functions

--- tests/testthat/test.print_msg.R
+++ tests/testthat/test.print_msg.R
@@ -1,10 +1,10 @@
 test_that("Just some check about print_msg", {
   msg_info <- capture.output(print_msg("Hello world!", "INFO"))
   expect_equal(msg_info, "|-- INFO :  Hello world! ")
-  
+
   msg_debug <- capture.output(print_msg("Hello world!", "DEBUG"))
   expect_equal(msg_debug, "|-- DEBUG :  Hello world! ")
-  
+
   msg_warning <- suppressWarnings(print_msg("Hello world!", "WARNING"))
   expect_equal(msg_warning, "|-- WARNING : Hello world!")
 })
--- tests/testthat/test.print_stat.R
+++ tests/testthat/test.print_stat.R
@@ -1,14 +1,35 @@
 test_that("Just some check about print_msg", {
-  options(warn=-1)
+  options(warn = -1)
   set.seed(123)
+
   data <- rnorm(3)
-  res <- print_stat("Summary statistics for data", data, round_val = 2, msg_type = "WARNING")
-  expect_equal(res, "|-- WARNING : Summary statistics for data: Min:-0.56 Q1:-0.4 Med:-0.23 Mean:0.26 Q3:0.66 Max:1.56")
+  res <- print_stat("Summary statistics for data",
+    data,
+    round_val = 2,
+    msg_type = "WARNING"
+  )
+  expect_equal(res, paste(
+    "|-- WARNING : Summary statistics for data:",
+    "Min:-0.56 Q1:-0.4 Med:-0.23",
+    "Mean:0.26 Q3:0.66 Max:1.56"
+  ))
+
   data <- rnorm(100)
-  res <- print_stat("Summary statistics for data", data, round_val = 1, msg_type = "WARNING")
-  
+  res <- print_stat("Summary statistics for data",
+    data,
+    round_val = 1,
+    msg_type = "WARNING"
+  )
+  expect_equal(res, paste(
+    "|-- WARNING : Summary statistics for data:",
+    "Min:-2.3 Q1:-0.5 Med:0.1 Mean:0.1 Q3:0.7 Max:2.2"
+  ))
+
   data <- letters[1:10]
-  res <- print_stat("Summary statistics for data", data, round_val = 1, msg_type = "WARNING")
+  res <- print_stat("Summary statistics for data",
+    data,
+    round_val = 1,
+    msg_type = "WARNING"
+  )
   expect_equal(res, "|-- WARNING : Summary statistics for data: No Statistics")
-  
 })
