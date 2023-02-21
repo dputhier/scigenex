23d363a323c456ab1bec9302bbe0d43bf8ad47b4
Julie BAVAIS <julie.bavais@yahoo.fr>
2023-02-21 10:33:29 +0100

    Update tests for print_msg function

--- tests/testthat/test.print_msg.R
+++ tests/testthat/test.print_msg.R
@@ -1,8 +1,10 @@
 test_that("Just some check about print_msg", {
-  res <- print_msg("Hello world!", "INFO")
-  expect_equal(res, NULL)
-  res <- print_msg("Hello world!", "DEBUG")
-  expect_equal(res, NULL)
-  res <- print_msg("Hello world!", "WARNING")
-  expect_equal(res, "|-- WARNING : Hello world!")  
+  msg_info <- capture.output(print_msg("Hello world!", "INFO"))
+  expect_equal(msg_info, "|-- INFO :  Hello world! ")
+  
+  msg_debug <- capture.output(print_msg("Hello world!", "DEBUG"))
+  expect_equal(msg_debug, "|-- DEBUG :  Hello world! ")
+  
+  msg_warning <- suppressWarnings(print_msg("Hello world!", "WARNING"))
+  expect_equal(msg_warning, "|-- WARNING : Hello world!")
 })
