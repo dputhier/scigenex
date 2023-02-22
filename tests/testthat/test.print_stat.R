5e3c8e9e2a5ad20443a486cdaafa813410d8557f
Julie BAVAIS <julie.bavais@yahoo.fr>
2023-02-22 13:56:59 +0100

    Fix tests for print_stat() and functions related to verbosity

--- tests/testthat/test.print_stat.R
+++ tests/testthat/test.print_stat.R
@@ -1,35 +1,43 @@
 test_that("Just some check about print_msg", {
-  options(warn = -1)
   set.seed(123)
 
   data <- rnorm(3)
-  res <- print_stat("Summary statistics for data",
-    data,
-    round_val = 2,
-    msg_type = "WARNING"
+  expect_warning(
+    print_stat(
+      "Summary statistics for data",
+      data,
+      round_val = 2,
+      msg_type = "WARNING"
+    ),
+    paste(
+      "|-- WARNING : Summary statistics for data:",
+      "Min:-0.56 Q1:-0.4 Med:-0.23",
+      "Mean:0.26 Q3:0.66 Max:1.56"
+    )
   )
-  expect_equal(res, paste(
-    "|-- WARNING : Summary statistics for data:",
-    "Min:-0.56 Q1:-0.4 Med:-0.23",
-    "Mean:0.26 Q3:0.66 Max:1.56"
-  ))
 
   data <- rnorm(100)
-  res <- print_stat("Summary statistics for data",
-    data,
-    round_val = 1,
-    msg_type = "WARNING"
+  expect_warning(
+    print_stat(
+      "Summary statistics for data",
+      data,
+      round_val = 1,
+      msg_type = "WARNING"
+    ),
+    paste(
+      "|-- WARNING : Summary statistics for data:",
+      "Min:-2.3 Q1:-0.5 Med:0.1 Mean:0.1 Q3:0.7 Max:2.2"
+    )
   )
-  expect_equal(res, paste(
-    "|-- WARNING : Summary statistics for data:",
-    "Min:-2.3 Q1:-0.5 Med:0.1 Mean:0.1 Q3:0.7 Max:2.2"
-  ))
 
   data <- letters[1:10]
-  res <- print_stat("Summary statistics for data",
-    data,
-    round_val = 1,
-    msg_type = "WARNING"
+  expect_warning(
+    print_stat(
+      "Summary statistics for data",
+      data,
+      round_val = 1,
+      msg_type = "WARNING"
+    ),
+    "|-- WARNING : Summary statistics for data: No Statistics"
   )
-  expect_equal(res, "|-- WARNING : Summary statistics for data: No Statistics")
 })
