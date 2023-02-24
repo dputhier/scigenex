7b8c6a861d7cc77f92999d594efbfbc62976f487
dputhier <puthier@gmail.com>
2023-02-24 18:24:57 +0100

    replace expect_equal(capture.output()) by expect_output()

--- tests/testthat/test.print_msg.R
+++ tests/testthat/test.print_msg.R
@@ -1,13 +1,13 @@
 test_that("Just some check about print_msg", {
   set_verbosity(2)
   
-  msg_info <- capture.output(print_msg("Hello world!", "INFO"))
-  expect_equal(msg_info, "|-- INFO :  Hello world! ")
+  expect_output(print_msg("Hello world!", "INFO"), 
+                          "|-- INFO :  Hello world! ")
 
-  msg_debug <- capture.output(print_msg("Hello world!", "DEBUG"))
-  expect_equal(msg_debug, "|-- DEBUG :  Hello world! ")
+  expect_output(print_msg("Hello world!", "DEBUG"),
+                          "|-- DEBUG :  Hello world! ")
 
   expect_warning(print_msg("Hello world!", "WARNING"),
                "|-- WARNING : Hello world!")
-  expect_equal(capture.output(print_msg("Hello world!")), "|-- INFO :  Hello world! ")
+  expect_output(print_msg("Hello world!"), "|-- INFO :  Hello world! ")
 })
