1e00e5e74cd93049943319a7edf871afa0e1eecf
dputhier <puthier@gmail.com>
2023-03-27 18:47:54 +0200

    Added few tests to rename()

--- tests/testthat/test.rename.R
+++ tests/testthat/test.rename.R
@@ -17,5 +17,8 @@ gn <- gene_clustering(res, keep_nn = TRUE, inflation = 1.1)
 test_that("Check 'rename' is working.", {
   expect_true(nclust(rename(gn, 1:nclust(gn))) == nclust(gn))
   expect_true(all(names(rename(gn, nclust(gn):1)@gene_clusters) == 4:1))
+  rn <- rename(gn, letters[1:nclust(gn)])
+  expect_is(plot_profiles(rn, ident = c(rep(1, 10), rep(2, 10))), "gg")
+  plot_heatmap(rn, cell_clusters = setNames(c(rep(1, 10), rep(2, 10)), col_names(rn)))
 })
 
