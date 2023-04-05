e4427bd5c38c47e8ef6912153a4290a7c370f9dc
dputhier <puthier@gmail.com>
2023-04-05 08:48:34 +0200

    Fix tests in which_clust()

--- tests/testthat/test.which_clust.R
+++ tests/testthat/test.which_clust.R
@@ -5,6 +5,6 @@ data("scigenex_test_I1.2")
 res <- scigenex_test_I1.2
 
 test_that("Check 'which_clust' is working.", {
-  expect_true(all(which_clust(gn, c('gene27', 'gene336', 'gene187')) == c(4,3,1)))
-  expect_true(all(is.na(which_clust(gn, c('gene27', 'gene336', 'bla'))) == c(F,F,T)))
+  expect_true(all(which_clust(res, c('gene27', 'gene336', 'gene187')) == c(4,3,1)))
+  expect_true(all(is.na(which_clust(res, c('gene27', 'gene336', 'bla'))) == c(F,F,T)))
 })
