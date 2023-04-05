255490e3f58ab39599bc6c5837938a15fbaf47d2
dputhier <puthier@gmail.com>
2023-04-05 08:33:33 +0200

    Fix text in reorder_genes(). Expose arguments for hierarchical clustering (nb_proc, link, method)

--- R/reorder_genes.R
+++ R/reorder_genes.R
@@ -1,60 +1,85 @@
-#################################################################
-##    Define the reorder_genes function
-#################################################################
-
-#' @title
-#' reorder_genes
+# -------------------------------------------------------------------------
+# Define the reorder_genes function ---------------------------------------
+# -------------------------------------------------------------------------
+#' @title Reorder genes in a ClusterSet object.
 #' @description
-#' Reorder genes in gene cluster.
+#' Reorder genes in a ClusterSet object according to various
+#' ordering rules.
 #' @param object A ClusterSet object.
-#' @param order_by A character string related to the reordering type. 
-#' Default is "correlation" which reorder genes based on their correlation 
-#' within their respective gene cluster.
+#' @param order_by A character string specifying the ordering rule. 
+#' Default is "gene_names" which reorder genes in alphanumerical 
+#' order. If 'hclust', hierarchical clustering is applied. If 
+#' 'correlation' the gene as ordered based on there similarity 
+#' with the cluster center.
+#' @param nb_proc The number of processor to be used if order_by=='hclust'.
+#' @param method The distance measure to be used order_by=='hclust'.
+#' @param link The agglomeration method to be used if order_by=='hclust'.
 #'
 #' @return ClusterSet-class object
-#' @export
+#' @export reorder_genes
+#' @keywords internal
+setGeneric("reorder_genes", 
+           function(object=NULL, 
+                    order_by = NULL,
+                    method=NULL,
+                    link=NULL,
+                    nb_proc=2)
+             standardGeneric("reorder_genes")
+)
+
+#' @title Reorder genes in a ClusterSet object.
+#' @description
+#' Reorder genes in a ClusterSet object according to various
+#' ordering rules.
+#' @param object A ClusterSet object.
+#' @param order_by A character string specifying the ordering rule. 
+#' Default is "gene_names" which reorder genes in alphanumerical 
+#' order. If 'hclust', hierarchical clustering is applied. If 
+#' 'correlation' the gene as ordered based on there similarity 
+#' with the cluster center.
+#' @param nb_proc The number of processor to be used if order_by=='hclust'.
+#' @param method The distance measure to be used order_by=='hclust'.
+#' @param link The agglomeration method to be used if order_by=='hclust'.
+#'
+#' @return ClusterSet-class object
+#' @importFrom amap hcluster
+#' @export reorder_genes
 #'
 #' @examples
 #' # Set verbosity to 1 to display info messages only.
 #' set_verbosity(1)
-#' 
-#' # Create a matrix with 4 signatures
-#' m <- create_4_rnd_clust()
-#' 
-#' # Select informative genes
-#' res <- select_genes(m,
-#'                     distance = "kendall",
-#'                     k = 75,
-#'                     highest = 0.3,
-#'                     fdr = 1e-8,
-#'                     row_sum = -Inf)
-#'                     
-#' # Cluster informative features
-#' res <- gene_clustering(res, 
-#'                        inflation = 1.2,
-#'                        keep_nn = FALSE,
-#'                        k = 5)
+#' # Load a dataset
+#' data("scigenex_test_I1.2")
 #'                        
 #' # Reorder genes using hierarchical clustering
-#' res <- reorder_genes(res, order_by = "hclust")
+#' scigenex_test_I1.2 <- reorder_genes(scigenex_test_I1.2, order_by = "hclust")
 #' 
 #' # Plot heatmap of gene clusters
-#' plot_heatmap(res)
+#' plot_heatmap(scigenex_test_I1.2)
 #' 
-#' @rdname reorder_genes
-
-reorder_genes <- function(object,
-                          order_by = c("gene_names", "hclust", "correlation")) {
+#' # Reorder and plot
+#' plot_heatmap(reorder_genes(scigenex_test_I1.2[1,], 
+#'                            order_by = "gene_names"), 
+#'              label_size = 5)
+#' 
+setMethod("reorder_genes", 
+          signature("ClusterSet"), 
+          function(object, 
+                   order_by = c("gene_names", "hclust", "correlation"),
+                   method=c("pearson", "maximum", "manhattan", "canberra", "binary", "euclidean", 
+                            "abspearson", "correlation", "abscorrelation", "spearman", "kendall"),
+                   link=c("average", "single", "complete", "ward", "mcquitty", "median", "centroid",
+                          "centroid2"),
+                   nb_proc=2) {
+  
+  order_by <- match.arg(order_by)
+  method <- match.arg(method)
+  link <- match.arg(link)
   
   ## Check format object arg
   check_format_cluster_set(object)
-  
-  if (!order_by %in% c("gene_names", "hclust", "correlation")) {
-    stop("Provided order_by parameter does not exist.")
-  }
-  
-  
-  reordered_genes <- c()
+
+  reordered_genes <- vector()
   for (gene_cluster in object@gene_clusters_metadata$cluster_id) {
     
     # Reorder by gene names
@@ -75,13 +100,12 @@ reorder_genes <- function(object,
       gene_cluster_names <- object@gene_clusters[[gene_cluster]]
       expression_matrix <- object@data[gene_cluster_names,]
       
-      
-      hclust_res <- hcluster(expression_matrix,
-                             method = "pearson",
+      hclust_res <- amap::hcluster(expression_matrix,
+                             method = method,
                              diag = FALSE,
                              upper = FALSE,
-                             link = "average",
-                             nbproc = 2,
+                             link = link,
+                             nbproc = nb_proc,
                              doubleprecision = TRUE)
       
       # Reorder genes in gene_clusters slot
@@ -108,4 +132,4 @@ reorder_genes <- function(object,
   
   
   return(object)
-}
+})
