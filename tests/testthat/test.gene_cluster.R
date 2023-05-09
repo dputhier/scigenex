eea7879ddead55e2d3598e096d7cefce6bea3b55
dputhier <puthier@gmail.com>
2023-05-09 16:01:11 +0200

    Fix tests

--- tests/testthat/test.gene_cluster.R
+++ tests/testthat/test.gene_cluster.R
@@ -3,21 +3,21 @@ test_that("Checking gene_cluster() is working...", {
   # Set verbosity to 0
   set_verbosity(0)
   
-  data("scigenex_test_I1.2")
-  res <- scigenex_test_I1.2
+  load_example_dataset("7871581/files/pbmc3k_medium_clusters")
+  res <- pbmc3k_medium_clusters
   
-  expect_equal(length(gene_cluster(res)), 359)
-  expect_equal(paste0(table(gene_cluster(res)), collapse = " "), "123 88 81 67")
-  expect_equal(as.vector(table(gene_cluster(res, cluster = 1))), 123)
-  expect_equal(as.vector(table(gene_cluster(res, cluster = 2))), 88)
-  expect_equal(as.vector(table(gene_cluster(res, cluster = 3))), 81)
-  expect_equal(as.vector(table(gene_cluster(res, cluster = 4))), 67)
-  expect_equal(as.vector(table(gene_cluster(res, cluster = c(1,4)))), c(123, 67))
-  expect_equal(as.vector(table(gene_cluster(res, cluster = c(4,4)))), 67)
+  expect_equal(length(gene_cluster(res)), 291)
+  expect_equal(paste0(table(gene_cluster(res)), collapse = " "), "51 49 45 24 20 18 14 14 14 12 7 7 6 5 5")
+  expect_equal(as.vector(table(gene_cluster(res, cluster = 1))), 51)
+  expect_equal(as.vector(table(gene_cluster(res, cluster = 2))), 49)
+  expect_equal(as.vector(table(gene_cluster(res, cluster = 3))), 45)
+  expect_equal(as.vector(table(gene_cluster(res, cluster = 4))), 24)
+  expect_equal(as.vector(table(gene_cluster(res, cluster = c(1,4)))), c(51, 24))
+  expect_equal(as.vector(table(gene_cluster(res, cluster = c(4,4)))), 24)
   expect_error(gene_cluster(res, cluster = c(-1,8)))
   expect_error(gene_cluster(res, cluster = c(0,9)))
   expect_error(gene_cluster(res, cluster = c(0:8)))
-  expect_error(gene_cluster(res, cluster = c(1:10)))
+  expect_error(gene_cluster(res, cluster = c(1,40)))
   expect_equal(paste0(res@gene_clusters[[1]], collapse = " "), 
                paste0(names(gene_cluster(res, cluster = 1)), collapse = " "))
   expect_equal(paste0(unlist(res@gene_clusters[1:2]), collapse = " "), 
