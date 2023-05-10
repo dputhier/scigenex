f2d8ff7fd4f950c33444725271ecb5598cb98c06
dputhier <puthier@gmail.com>
2023-05-10 11:26:21 +0200

    deleted ref to scigenex_test_I1.2

--- tests/testthat/test.rename.R
+++ tests/testthat/test.rename.R
@@ -1,19 +1,15 @@
 # Set verbosity to 0
 set_verbosity(0)
+library(Seurat)
 
-#Create matrix containing 3 signatures
-m <- create_4_rnd_clust()
-
-## Select informative genes
-
-data("scigenex_test_I1.2")
-gn <- scigenex_test_I1.2
+load_example_dataset("7871581/files/pbmc3k_medium_clusters")
+load_example_dataset("7871581/files/pbmc3k_medium")
+gn <- pbmc3k_medium_clusters
 
 test_that("Check 'rename' is working.", {
   expect_true(nclust(rename_clust(gn, 1:nclust(gn))) == nclust(gn))
-  expect_true(all(names(rename_clust(gn, nclust(gn):1)@gene_clusters) == 4:1))
+  expect_true(all(names(rename_clust(gn, nclust(gn):1)@gene_clusters) == 15:1))
   rn <- rename_clust(gn, letters[1:nclust(gn)])
-  expect_is(plot_profiles(rn, ident = c(rep(1, 10), rep(2, 10))), "gg")
-  plot_heatmap(rn, cell_clusters = setNames(c(rep(1, 10), rep(2, 10)), col_names(rn)))
+  expect_is(plot_profiles(rn, ident = Seurat::Idents(pbmc3k_medium)), "gg")
 })
 
