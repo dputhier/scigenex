168e16583f260edfdeeabf7fc64ac64cc94294ad
dputhier <puthier@gmail.com>
2023-11-13 15:44:18 +0100

    Fix #134

--- R/clusterset_report.R
+++ R/clusterset_report.R
@@ -1,4 +1,5 @@
 #' @title Create a report from a ClusterSet and Seurat object.
+#' @description Create a report from a ClusterSet and Seurat object.
 #' @param clusterset_object The ClusterSet object.
 #' @param seurat_object The Seurat object.
 #' @param file_path A file path where to store the report (html extension).
@@ -13,6 +14,7 @@
 #' "GeneRatio", "BgRatio", "pvalue", "p.adjust", "qvalue", "geneID", "Count"
 #' @param verbosity The verbosity level (0-3, defaut to 1.)
 #' @param pandoc_heap_size Fix the maximum memory used by pandoc. See https://pandoc.org/MANUAL.html.
+#' @param report_id Used to label code chunks.
 #' @examples 
 #' # Load datasets
 #' # load_example_dataset("8028126/files/pbmc3k_medium_clusters_enr")
@@ -37,14 +39,15 @@ cluster_set_report <- function(clusterset_object=NULL,
                                pt_size=2.75,
                                go_info=c("ID", "Description", "GeneRatio",  "pvalue", "qvalue", "Count"),
                                verbosity=1,
-                               pandoc_heap_size ="512m") {
-
-  check_format_cluster_set(clusterset_object)
+                               pandoc_heap_size ="512m",
+                               report_id=rlang::hash(clusterset_object)) {
   
+  check_format_cluster_set(clusterset_object)
+  print_msg(paste0("Report ID is : ", report_id), msg_type = "DEBUG")
   print_msg("Preparing report.")
-
+  
   set_verbosity(verbosity)
-
+  
   if(! all(go_info %in% c("ONTOLOGY", "ID", "Description", 
                           "GeneRatio", "BgRatio", "pvalue", 
                           "p.adjust", "qvalue", "geneID", "Count"))){
@@ -56,8 +59,8 @@ cluster_set_report <- function(clusterset_object=NULL,
   }else{
     heatmap_color_ident <- "NULL"
   }
-    
-
+  
+  
   # Dealing with file path
   
   if(!is.null(file_path)){
@@ -121,7 +124,7 @@ params:
 </style>
 
 
-```{r, echo=FALSE, result='hide', message=FALSE, warning=FALSE }
+```{r {{report_id}}_addModuleScore, echo=FALSE, result='hide', message=FALSE, warning=FALSE }
 set_verbosity(0)
 load("{{data_path}}")
 clusterset_object <- top_genes(clusterset_object)
@@ -157,14 +160,14 @@ if("Spatial" %in% names(seurat_object@assays)){
 <div class = "row">
 <div class = "column">
 
-```{r, echo=FALSE, result='hide', message=FALSE, warning=FALSE }
+```{r {{report_id}}_dimplot, echo=FALSE, result='hide', message=FALSE, warning=FALSE }
 DimPlot(seurat_object, reduction = "umap", label = TRUE)
 ```
 
 </div>
 <div class = "column">
 
-```{r, echo=FALSE, result='hide', message=FALSE, warning=FALSE }
+```{r {{report_id}}_spatialdimplot, echo=FALSE, result='hide', message=FALSE, warning=FALSE }
 if("Spatial" %in% names(seurat_object@assays)){
   SpatialDimPlot(seurat_object, label = TRUE, label.size = 3, pt.size.factor = 1.4)
 }
@@ -176,7 +179,7 @@ if("Spatial" %in% names(seurat_object@assays)){
 <div class = "row">
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE}
+```{r {{report_id}}_plot_ggheatmap, echo=FALSE, message=FALSE, warning=FALSE}
 plot_ggheatmap(clusterset_object, 
                use_top_genes = TRUE,
                colors = {{heatmap_colors}},
@@ -188,7 +191,7 @@ plot_ggheatmap(clusterset_object,
 </div>
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE}
+```{r {{report_id}}_empty, echo=FALSE, message=FALSE, warning=FALSE}
 
 ```
 
@@ -199,7 +202,7 @@ plot_ggheatmap(clusterset_object,
 <div class = "row">
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE}
+```{r {{report_id}}_datatable, echo=FALSE, message=FALSE, warning=FALSE}
 df_tmp <- data.frame(gene_cluster(clusterset_object))
 DT::datatable(data.frame(Gene=rownames(df_tmp), 
                          Module=gene_cluster(clusterset_object)),
@@ -219,7 +222,7 @@ clust_code <- r"(
 <div class = "row">
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE}
+```{r {{report_id}}_{{clust_number}}_moduleinfo, echo=FALSE, message=FALSE, warning=FALSE}
 clust_sub <- clusterset_object[{{clust_number}},]
 gene <- clust_sub@gene_clusters[[1]]
 data_sub <- clust_sub@data
@@ -239,7 +242,7 @@ DT::datatable(data.frame(Gene=gene,
 
 <div class = "column">
 
-```{r echo=FALSE, message=FALSE, warning=FALSE, result='hide'  }
+```{r {{report_id}}_{{clust_number}}_plot_spatial, echo=FALSE, message=FALSE, warning=FALSE, result='hide'  }
 if("Spatial" %in% names(seurat_object@assays)){
  plot_spatial(seurat_obj = seurat_object, 
              metadata = "Cluster_{{clust_number}}", 
@@ -252,7 +255,7 @@ if("Spatial" %in% names(seurat_object@assays)){
 <div class = "row">
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE}
+```{r {{report_id}}_{{clust_number}}_plot_heatmap_2, echo=FALSE, message=FALSE, warning=FALSE}
 plot_heatmap(clusterset_object[{{clust_number}}, ], 
                use_top_genes = TRUE,
                colors = {{heatmap_colors}},  
@@ -263,7 +266,7 @@ plot_heatmap(clusterset_object[{{clust_number}}, ],
 
 <div class = "column">
 
-```{r echo=FALSE, message=FALSE, warning=FALSE, result='hide'  }
+```{r {{report_id}}_{{clust_number}}_empty_2, echo=FALSE, message=FALSE, warning=FALSE, result='hide'  }
 
 ```
 </div>
@@ -273,13 +276,13 @@ plot_heatmap(clusterset_object[{{clust_number}}, ],
 <div class = "row">
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE } 
+```{r {{report_id}}_{{clust_number}}_empty_3, echo=FALSE, message=FALSE, warning=FALSE } 
 
 ```
 </div>
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE }
+```{r {{report_id}}_{{clust_number}}_empty_4, echo=FALSE, message=FALSE, warning=FALSE }
 
 ```
 
@@ -289,7 +292,7 @@ plot_heatmap(clusterset_object[{{clust_number}}, ],
 <div class = "row">
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE }
+```{r {{report_id}}_{{clust_number}}_datatable_2, echo=FALSE, message=FALSE, warning=FALSE }
 if(length(clusterset_object@gene_cluster_annotations) > 0){
   DT::datatable(clusterset_object@gene_cluster_annotations[[{{clust_number}}]]@result[, {{go_info}}],
               rownames= FALSE)
@@ -298,7 +301,7 @@ if(length(clusterset_object@gene_cluster_annotations) > 0){
 </div>
 <div class = "column">
 
-```{r, echo=FALSE, message=FALSE, warning=FALSE }
+```{r {{report_id}}_{{clust_number}}_empty_5, echo=FALSE, message=FALSE, warning=FALSE }
 
 ```
 
@@ -307,53 +310,55 @@ if(length(clusterset_object@gene_cluster_annotations) > 0){
 
 
 )"
-  
-  rmd_code <- jinjar::render(rmd_code, 
+
+print_msg(msg = "Rendering main.", msg_type = "DEBUG")
+rmd_code <- jinjar::render(rmd_code, 
                            title=report_title,
                            report_author=report_author,
                            data_path=data_path,
                            heatmap_colors=capture.output(dput(heatmap_colors))[[1]],
                            heatmap_color_ident=heatmap_color_ident,
-                           pandoc_heap_size=pandoc_heap_size
+                           pandoc_heap_size=pandoc_heap_size,
+                           report_id=report_id
+)
+
+
+
+capt_heatmap_colors <- capture.output(dput(heatmap_colors))[[1]]
+capt_go_info <- capture.output(dput(go_info))
+capt_go_info <- paste0(capt_go_info, collapse = "")
+coord_flip <- ifelse(coord_flip, "TRUE", "FALSE")
+
+
+print_msg(msg = "Rendering clusters", msg_type = "DEBUG")
+
+for(clust_number in 1:nclust(clusterset_object)){
+  
+  glue_code <- jinjar::render(clust_code,
+                              heatmap_colors=capt_heatmap_colors,
+                              go_info=capt_go_info,
+                              pt_size=pt_size,
+                              coord_flip=coord_flip,
+                              heatmap_color_ident=heatmap_color_ident,
+                              clust_number=clust_number,
+                              report_id=report_id
   )
+  
+  
+  rmd_code <- paste0(rmd_code, 
+                     glue_code,
+                     collapse = "")
+}
 
+rmd_file <- gsub(pattern = ".html$", ".Rmd", file_path)
 
-  
-  capt_heatmap_colors <- capture.output(dput(heatmap_colors))[[1]]
-  capt_go_info <- capture.output(dput(go_info))
-  capt_go_info <- paste0(capt_go_info, collapse = "")
-  coord_flip <- ifelse(coord_flip, "TRUE", "FALSE")
-  
+print_msg(paste0("Writing report into ", rmd_file))
+writeLines(rmd_code, con = rmd_file)
 
-  
-  
-  for(clust_number in 1:nclust(clusterset_object)){
-
-    glue_code <- jinjar::render(clust_code,
-                                 heatmap_colors=capt_heatmap_colors,
-                                 go_info=capt_go_info,
-                                 pt_size=pt_size,
-                                 coord_flip=coord_flip,
-                                 heatmap_color_ident=heatmap_color_ident,
-                                clust_number=clust_number
-    )
-    
-    
-    rmd_code <- paste0(rmd_code, 
-                       glue_code,
-                        collapse = "")
-  }
-
-  
-  rmd_file <- gsub(pattern = ".html$", ".Rmd", file_path)
-  
-  print_msg(paste0("Writing report into ", rmd_file))
-  writeLines(rmd_code, con = rmd_file)
-  
-  print_msg(paste0("Rmd file written at : ", rmd_file))
-  print_msg("Rendering report.")
-  print_msg(paste0("Creating report at : ", file_path))
-  rmarkdown::render(rmd_file, quiet = TRUE)
+print_msg(paste0("Rmd file written at : ", rmd_file))
+print_msg("Rendering report.")
+print_msg(paste0("Creating report at : ", file_path))
+rmarkdown::render(rmd_file, quiet = TRUE)
 
 }
 
