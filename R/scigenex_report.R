6b6bf900ca2c905ca5b60a9593a75419006d5447
dputhier <puthier@gmail.com>
2025-05-09 17:27:53 +0200

    added scigenex_report()

--- R/scigenex_report.R
+++ R/scigenex_report.R
@@ -3,21 +3,23 @@
 #===========================================================
 #' @title Create a report from a ClusterSet experiment.
 #' @description
-#' Create a report from a ClusterSet experiment and Seurat object. If Seurat contains spatial information set 'spatial' argument to TRUE and selection the 
-#' corresponding assay and layer. The function call Gemini IA to try to guess cell type and functions associated to each cluster.
+#' Create a report from a ClusterSet experiment and Seurat object. If is_spatial_exp argument is set to TRUE Spatial diagram will be added. 
+#' The function can call Gemini IA to try to guess cell type and functions associated to each gene module.
 #' @param cluster_set A clusterSet object.
 #' @param seurat_object Seurat object.
 #' @param seurat_assay Which assay should be used in the Seurat object.
 #' @param seurat_layer Which layer should be used in the Seurat object.
-#' @param spatial_experiment Whether there are some spatial information in the ClusterSet object.
+#' @param is_spatial_exp Whether there are some spatial information in the ClusterSet object.
 #' @param report_title A title for the report.
 #' @param report_subtitle A subtitle for the report.
-#' @param report_author A character string corresponding to one or severe al authors.
+#' @param report_author A character string corresponding to one or several authors.
 #' @param report_date A date.
 #' @param out_dir A directory where to store the bookdown output.
 #' @param experimenters Name of the experimenter.
 #' @param experimenter_labs Laboratory of the experimenter.
-#' @param api_key An API key for Gemini to perform cell type / cell function analysis. Otherwise deactivate section 'module_cell_annot_IA'.
+#' @param bioc_org_db A gene annotation database as provided by Bioconductor (e.g. org.Hs.eg.db, org.Mm.eg.db, org.Rn.eg.db, o
+#' rg.Dm.eg.db, org.Dr.eg.db, org.Sc.sgd.db, org.Ce.eg.db...). If NULL, module analysis related to functional annotation are canceled.
+#' @param api_key An API key for Gemini to perform cell type / cell function analysis. If NULL, the 'module_cell_annot_IA' section will be skipped.
 #' @param smp_species The species of the sample (free text). E.g 'Homo sapiens'.
 #' @param smp_stage Development stage of the sample (free text). E.g. 'adult'.
 #' @param smp_organ The sample organ (free text). E.g. 'tonsil'.
@@ -25,30 +27,68 @@
 #' @param rmd_dir A path where to find the templates for creating the book.
 #' @param rm_tmpdir Whether to delete temporary directory.
 #' @param quiet Whether to run bookdown::render_book() quietly.
+#' @return No return value. A report is generated and written to the specified output directory.
 #' @examples
-#' ## TODO:  'org.Hs.eg.db' / enrich_go "Hsapiens" / library(enrichplot) / GOSemSim::godata / patchwork /xaringanExtra / "/Users/puthier/Documents/git/project_dev/scigenex"
+#' ## TODO:  'org.Hs.eg.db' / enrich_go "Hsapiens" / library(enrichplot) / xaringanExtra / GOSemSim::godata / patchwork /xaringanExtra / "/Users/puthier/Documents/git/project_dev/scigenex"
 #' library(scigenex)
 #' library(Seurat)
-#' library(clusterProfiler)
-#' library(enrichplot)
+#' set_verbosity(3)
 #' load_example_dataset('7871581/files/pbmc3k_medium_clusters')
 #' load_example_dataset('7871581/files/pbmc3k_medium')
-#' scigenex_report(pbmc3k_medium_clusters, 
+#' scigenex_report(pbmc3k_medium_clusters[1:3,], 
 #'                 pbmc3k_medium, 
 #'                 smp_species="Homo_sapiens", 
 #'                 smp_region="total", 
 #'                 smp_organ="blood", 
+#'                 bioc_org_db="org.Hs.eg.db",
 #'                 smp_stage="adult", rmd_dir="/Users/puthier/Documents/git/project_dev/scigenex/inst/rmarkdown", 
 #'                 api_key="AIzaSyDj2dA0w4LoXi6LhXgf62vEOkuOUnpONbY")
-#' @export scigenex_report
+#' set_verbosity(3)
+#' load_example_dataset('7870305/files/lymph_node_tiny_clusters_2')
+#' load_example_dataset('7870305/files/lymph_node_tiny_2')
+#' scigenex_report(lymph_node_tiny_clusters_2[1:3,], 
+#'                 lymph_node_tiny_2, 
+#'                 smp_species="Homo sapiens", 
+#'                 smp_region="total", 
+#'                 smp_organ="lymph node", 
+#'                 smp_stage="adult", 
+#'                 bioc_org_db="org.Hs.eg.db",
+#'                 rmd_dir="/Users/puthier/Documents/git/project_dev/scigenex/inst/rmarkdown", 
+#'                 api_key="AIzaSyDj2dA0w4LoXi6LhXgf62vEOkuOUnpONbY",
+#'                 is_spatial_exp=TRUE,
+#'                 SpatialFeaturePlot_params=list(pt.size.factor = 3000)) # Object was created with an older seurat version
 #' @importFrom fs path_home
-#' @importFrom Seurat AddModuleScore
 #' @importFrom bookdown render_book
+#' @importFrom xaringanExtra use_panelset
+#' @importFrom xaringanExtra style_panelset
+#' @importFrom xaringanExtra style_panelset_tabs
+#' @importFrom BiocManager install
+#' @importFrom knitr opts_chunk
+#' @importFrom knitr asis_output
+#' @importFrom Seurat VlnPlot
+#' @importFrom gemini.R setAPI
+#' @importFrom gemini.R gemini_chat
+#' @importFrom AnnotationDbi select
+#' @importFrom dplyr mutate
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_text
+#' @importFrom clusterProfiler enrichGO
+#' @importFrom enrichplot pairwise_termsim
+#' @importFrom enrichplot dotplot
+#' @importFrom enrichplot cnetplot
+#' @importFrom Seurat NoLegend
+#' @importFrom Seurat SpatialFeaturePlot
+#' @importFrom Seurat FeaturePlot
+#' @importFrom Seurat AddModuleScore
+#' @importFrom dplyr "%>%"
+#' @importFrom pander pander
+#' @importFrom GOSemSim godata
+#' @export
 scigenex_report <- function(cluster_set = NULL,
                             seurat_object=NULL,
                             seurat_assay=NULL,
                             seurat_layer=NULL,
-                            spatial_experiment=FALSE,
+                            is_spatial_exp=FALSE,
                             report_title = "Spatial transcriptomics report",
                             report_subtitle = "An example experiment",
                             report_author = "Undefined",
@@ -56,24 +96,42 @@ scigenex_report <- function(cluster_set = NULL,
                             out_dir = file.path(fs::path_home(), "scigenex_book"),
                             experimenters = NULL,
                             experimenter_labs=NULL,
+                            bioc_org_db=NULL,
                             api_key=NULL,
                             smp_species=NULL,
                             smp_stage=NULL,
                             smp_organ=NULL,
                             smp_region=NULL,
                             rmd_dir = file.path(system.file(package = "scigenex"), 
-                                                                      "rmarkdown"),
-                            add_module_score_params=list(pool = NULL,
-                                                         nbin = 16,
+                                                "rmarkdown"),
+                            add_module_score_params=list(nbin = 16,
                                                          ctrl = 100,
-                                                         k = FALSE,
-                                                         assay = NULL,
                                                          name = "MOD_",
-                                                         seed = 1,
-                                                         search = FALSE,
                                                          slot = "data"),
+                            FeaturePlot_params=list(cols=c("lightgrey", "#ff0000", "#00ff00")),
+                            SpatialFeaturePlot_params=list(pt.size.factor = 1.7),
+                            plot_ggheatmap_params=list(use_top_genes=FALSE, 
+                                                       hide_gene_name=TRUE),
+                            plot_heatmap_params=list(link="complete", 
+                                                     use_top_genes=FALSE,
+                                                     interactive=TRUE,
+                                                     line_size_horizontal=1,
+                                                     label_size=7),
+                            cnetplot_params=list(color.params = list(edge = TRUE), 
+                                                 showCategory = 6, 
+                                                 cex.params = list(category_label = 0.6, 
+                                                                   gene_label = 0.7)),
                             rm_tmpdir = TRUE,
-                            section=c("exp_info", 
+                            section=c("exp_info",
+                                      "exp_metadata",
+                                      "exp_experimenters",
+                                      "exp_sample",
+                                      "exp_params", 
+                                      "exp_stats",
+                                      "exp_genes",
+                                      "exp_heatmap",
+                                      "exp_spatial",
+                                      "module_spatial",
                                       "module_heatmap",
                                       "module_iheatmap",
                                       "module_umap",
@@ -88,40 +146,65 @@ scigenex_report <- function(cluster_set = NULL,
   
   verb_level <- get_verbosity()
   
+  if(!require(dplyr, quietly = TRUE)) print_msg("Dplyr is required", msg_type = "STOP")
+  
   if("module_cell_annot_IA" %in% section){
     if(is.null(smp_species) | is.null(smp_stage) | is.null(smp_organ) | is.null(smp_region))
       print_msg("Please provide information about species, stage, organ and region for IA-based annotation.")
   }
-
-  if("module_cell_annot_IA" %in% section){
-    if(is.null(api_key))
-      print_msg("Please provide a Gemini API key.", msg_type='STOP')
-    
-    print_msg("Setting Gemini API key.", msg_type='INFO')
-    gemini.R::setAPI(api_key)
-  }
-    
+  
+  print_msg("Checking seurat object.", msg_type="DEBUG") 
+  
+  if (!inherits(seurat_object, "Seurat"))
+    print_msg("Please provide a valid Seurat object.", msg="STOP") 
+  
   check_format_cluster_set(cluster_set)
+  
   
   if(nclust(cluster_set) < 1 ) 
     print_msg("No cluster found in ClusterSet.", msg_type="STOP")
   
-  print_msg("Checking seurat object.", msg_type="DEBUG") 
-  
-  if(!is.null(seurat_object)){
-
-    if (!inherits(seurat_object, "Seurat"))
-      print_msg("Please provide a valid Seurat object.", msg="STOP") 
-    
-    if(length(Idents(seurat_object)) == 0 )
-      print_msg("Seurat object needs to contain cll identities (check with Idents()).", msg="STOP")
-    
-    add_module_score_params$object <- seurat_object
-    add_module_score_params$features <- cluster_set@gene_clusters
-    add_module_score_params$name <- "MOD_"
-    seurat_object <- do.call(Seurat::AddModuleScore, add_module_score_params)
+  if(is.null(bioc_org_db)){
+    print_msg("No annotation database found for organism...", msg_type = "WARNING")
+    print_msg("Canceling functional annotation", msg_type = "WARNING")
+    section <- setdiff(section, c("cmp_term_network",
+                                  "cmp_term_barplot_1",
+                                  "cmp_term_barplot_2",
+                                  "cmp_term_network_circ"))
+  }else{
+    if(!require(bioc_org_db, character.only = TRUE, quietly=TRUE)){
+      print_msg("The annotation library (see 'bioc_org_db') was not found. Please install it.", msg_type = "STOP")
+    }else{
+      library(bioc_org_db, character.only = TRUE)
+    }
   }
-
+  
+  if(!is_spatial_exp){
+    print_msg("This is not a ST experiment...", msg_type = "INFO")
+    print_msg("Canceling ST reporting module.", msg_type = "INFO")
+    section <- setdiff(section, c("exp_spatial",
+                                  "module_spatial"))
+  }
+  
+  if(is.null(api_key)){
+    print_msg("Non Gemini key provided...", msg_type = "INFO")
+    print_msg("Canceling IA-based cell type annotation.", msg_type = "INFO")
+    section <- setdiff(section, c("exp_spatial",
+                                  "module_cell_annot_IA"))
+  }
+  
+  
+  if(length(Idents(seurat_object)) == 0 )
+    print_msg("Seurat object needs to contain all identities (check with Idents()).", msg="STOP")
+  
+  add_module_score_used_params <- as.list(formals(Seurat:::AddModuleScore.Seurat))
+  for(i in names(add_module_score_params)){add_module_score_used_params[[i]] <- add_module_score_params[[i]]}
+  
+  add_module_score_used_params$object <- seurat_object
+  add_module_score_used_params$features <- cluster_set@gene_clusters
+  add_module_score_used_params$name <- "MOD_"
+  seurat_object <- do.call(Seurat::AddModuleScore, add_module_score_used_params)
+  
   tmp_dir <- tempdir(check = FALSE)
   tmp_dir <- paste0(tmp_dir, format(Sys.time(), "%a-%b-%e-%H-%M-%S-%Y"))
   dir.create(tmp_dir, showWarnings = FALSE, recursive = TRUE)
@@ -134,19 +217,20 @@ scigenex_report <- function(cluster_set = NULL,
   out <- file.copy(rmd_dir, tmp_dir, recursive = TRUE)
   
   tmp_dir <- file.path(tmp_dir, "rmarkdown")
-
+  
   # Compute top genes
   cluster_set <- top_genes(cluster_set)
   
   if(any(c("module_term_network",
-         "module_term_barplot_1",
-         "module_term_barplot_2",
-         "module_term_network_circ",
-         "term_table") %in% section)){
+           "module_term_barplot_1",
+           "module_term_barplot_2",
+           "module_term_network_circ",
+           "term_table") %in% section)){
     print_msg("Computing semantic similarity for cnet_plot.", msg_type="INFO") 
-    suppressMessages(sem_sim <- GOSemSim::godata('org.Hs.eg.db', ont="BP"))
+    suppressMessages(sem_sim <- GOSemSim::godata(annoDb=eval(bioc_org_db),
+                                                 ont="BP"))
   }
-
+  
   
   module_rmd <- file.path(tmp_dir, "module.rmd")
   
@@ -155,6 +239,7 @@ scigenex_report <- function(cluster_set = NULL,
   print_msg("Looping through parameterised Reports.")
   
   n <- 1
+  
   all_knited_files <- vector()
   
   for(n in 1:nclust(cluster_set)){
@@ -169,15 +254,21 @@ scigenex_report <- function(cluster_set = NULL,
     code_rmd  <- readLines(module_rmd)
     code_rmd  <- gsub("FIG_PATH", fig_path, x = code_rmd)
     code_rmd  <- gsub("MODULE_NUMBER", n, x = code_rmd)
+    
+    if(!is.null(api_key)){
+      print_msg("Setting Gemini API key.", msg_type='INFO')
+      code_rmd  <- gsub("APIKEY", api_key, x = code_rmd)
+    }
+    
     writeLines(code_rmd, con = cur_rmd)
     
     print_msg(paste0("Preparation of rmd files for objects", n, "finished."), msg_type = "DEBUG")
     
     all_knited_files[n] <- basename(cur_rmd)
-
+    
   }
   
-
+  
   print_msg("Deleting sample.Rmd.")
   
   unlink(module_rmd)
@@ -197,9 +288,8 @@ scigenex_report <- function(cluster_set = NULL,
     ),
     x = code_yml
   )
-
+  
   writeLines(code_yml, con = file.path(tmp_dir, "_bookdown.yml"))
-  code_yml  <- readLines(file.path(tmp_dir, "_bookdown.yml"), warn = FALSE)
   
   options(knitr.duplicate.label = "allow")
   
@@ -208,7 +298,16 @@ scigenex_report <- function(cluster_set = NULL,
   
   dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
   
-  file.copy(file.path(tmp_dir, "_book"), out_dir, recursive=TRUE)
+  for(tpfile in list.files(file.path(tmp_dir, "_book")))
+    file.copy(file.path(tmp_dir, "_book", tpfile), out_dir, recursive=TRUE)
+  
+  dir.create(file.path(out_dir, "rmarkdown"), recursive = TRUE, showWarnings = FALSE)  
+  
+  rmd_input <- list.files(file.path(tmp_dir))
+  rmd_input <-   rmd_input[rmd_input != "_book"]              
+  
+  for(tpfile in rmd_input)
+    file.copy(file.path(tmp_dir, tpfile), file.path(out_dir, "rmarkdown"), recursive=TRUE)
   
   print_msg(paste0("Results have been copied to:", out_dir))
   
